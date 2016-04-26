6e241d250c70636f8149271ee70557fbbaee4cec
Tom Emmerson <thomasemmerson@sky.com>
2016-04-26 16:29:04 +0100

    dddd

--- pages/contact/controllers/contact.js
+++ pages/contact/controllers/contact.js
@@ -5,16 +5,16 @@ angular.module('scotchApp')
 	// create the controller and inject Angular's $scope
 	.controller('contactController', function($scope, dataService) {
 
-
-		//get absences list upon pageload
-    	dataService.getAbsences(function(response) {
-        	$scope.absences = response.data;
-    	});
-
-		//$scope.absence = {"name" : "", "start_date" : "", "start_time" : "", "end_date" : "", "end_time" : "", "type" : "", "notes" : "", "status" : ""},
+		$scope.absence = {"name" : "",
+			"start_date" : "",
+			"start_time" : "",
+			"end_date" : "",
+			"end_time" : "",
+			"type" : "",
+			"notes" : "",
+			"status" : ""};
 
 		$scope.reset = function(absence){
-	
 			$scope.absence.name = "";	
 			$scope.absence.type = "";
 			$scope.absence.notes = "";
@@ -23,11 +23,10 @@ angular.module('scotchApp')
 			$scope.absence.end_date = "";	
 			$scope.absence.end_time = "";
 			$scope.absence.notes = "";	
-
 		}
 
       	$scope.update = function(absence) {
-      		$scope.absence.push({name : $scope.name, start_date: $scope.start_date, start_time: $scope.start_time, end_date: $scope.end_date, end_time: $scope.end_time, type: $scope.type, notes: $scope.notes, "status" : "Requested"});
+      		console.log("This will push another object into the array.");
       	};
 
     })
