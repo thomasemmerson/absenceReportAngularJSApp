64dbcdacb70f910fcda8b992d7217eceae7f4a95
Tom Emmerson <thomasemmerson@sky.com>
2016-04-26 16:23:11 +0100

    Revert "this too"
    This reverts commit a595d74fd6542df18a11384ea3f3d10f1e73551c.

--- pages/home/controllers/home.js
+++ pages/home/controllers/home.js
@@ -3,8 +3,12 @@
 angular.module('scotchApp')
 
 	// create the controller and inject Angular's $scope
-	.controller('mainController', function($scope) {
+	.controller('mainController', function($scope, dataService) {
 
+	    //get absences list upon pageload
+    	dataService.getAbsences(function(response) {
+        	$scope.absences = response.data;
 
+    	});
 
 	})
