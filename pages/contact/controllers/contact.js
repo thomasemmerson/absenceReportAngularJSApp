d90934cf6d6531c162ffb4716f7bc039ef88422b
Tom Emmerson <thomasemmerson@sky.com>
2016-04-26 16:16:11 +0100

    update all working

--- pages/contact/controllers/contact.js
+++ pages/contact/controllers/contact.js
@@ -6,13 +6,6 @@ angular.module('scotchApp')
 	.controller('contactController', function($scope, dataService) {
 
 
-		//get absences list upon pageload
-    	dataService.getAbsences(function(response) {
-        	$scope.absences = response.data;
-    	});
-
-		//$scope.absence = {"name" : "", "start_date" : "", "start_time" : "", "end_date" : "", "end_time" : "", "type" : "", "notes" : "", "status" : ""},
-
 		$scope.reset = function(absence){
 	
 			$scope.absence.name = "";	
@@ -27,7 +20,8 @@ angular.module('scotchApp')
 		}
 
       	$scope.update = function(absence) {
-      		$scope.absence.push({name : $scope.name, start_date: $scope.start_date, start_time: $scope.start_time, end_date: $scope.end_date, end_time: $scope.end_time, type: $scope.type, notes: $scope.notes, "status" : "Requested"});
+      		console.log("Function to push new object into array will fire here.");
       	};
+      	
 
     })
